e131f8bc9de3ee43e352bdb11a380fb943a5b50c
cajomitara <cajomitara@users.noreply.github.com>
2025-03-11 20:31:30 +0800

    Дозагружен код из консольного задания

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            textC = new TextBox();
+            textB = new TextBox();
+            textA = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(575, 120);
+            label1.TabIndex = 0;
+            label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(181, 138);
+            label2.Name = "label2";
+            label2.Size = new Size(238, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Введите сумму денег на счёте A: ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(98, 205);
+            label3.Name = "label3";
+            label3.Size = new Size(445, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Введите необходимую величину ежемесячного увеличения B:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(124, 277);
+            label4.Name = "label4";
+            label4.Size = new Size(376, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Введите сумму C, которую нужно превысить вкладу: ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(235, 346);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 41);
+            button1.TabIndex = 4;
+            button1.Text = "Посчитать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textC
+            // 
+            textC.Location = new Point(181, 300);
+            textC.Name = "textC";
+            textC.Size = new Size(238, 27);
+            textC.TabIndex = 5;
+            // 
+            // textB
+            // 
+            textB.Location = new Point(181, 228);
+            textB.Name = "textB";
+            textB.Size = new Size(238, 27);
+            textB.TabIndex = 6;
+            // 
+            // textA
+            // 
+            textA.Location = new Point(181, 161);
+            textA.Name = "textA";
+            textA.Size = new Size(238, 27);
+            textA.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(601, 450);
+            Controls.Add(textA);
+            Controls.Add(textB);
+            Controls.Add(textC);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Калькулятор денег";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private TextBox textC;
+        private TextBox textB;
+        private TextBox textA;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,116 @@ namespace lab2_task2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                string input_A;
+                string input_B;
+                string input_C;
+
+                string output;
+
+                do
+                {
+                    Console.Write("¬ведите сумму денег на счЄте A: ");
+                    input_A = Console.ReadLine();
+
+                    Console.Write("¬ведите необходимую величину ежемес€чного увеличени€ B: ");
+                    input_B = Console.ReadLine();
+
+                    Console.Write("¬ведите сумму C, которую нужно превысить вкладу: ");
+                    input_C = Console.ReadLine();
+                    Console.WriteLine();
+                } while (!isInputValid(input_A) || !isInputValid(input_B) || !isInputValid(input_C));
+
+                output = Logic.FindMonths(input_A, input_B, input_C);
+
+                Console.WriteLine(output);
+        }
+        public static bool isInputValid(string input)
+        {
+            if (double.TryParse(input, out double amount))
+            {
+                if (amount <= 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+    public class Logic
+    {
+        public static string FindMonths(string input_A, string input_B, string input_C)
+        {
+            string output;
+
+            double money_A = double.Parse(input_A);
+            double money_B = double.Parse(input_B);
+            double money_C = double.Parse(input_C);
+
+            int month_B = 0;
+            int month_C = 0;
+            double currentBalance_B = money_A;
+            double currentBalance_C = money_A;
+
+            bool bFound = false;
+            bool cFound = false;
+
+            while (!bFound)
+            {
+                month_B++;
+
+                double monthlyIncrease = currentBalance_B * 0.02;
+
+                if (monthlyIncrease > money_B)
+                {
+                    bFound = true;
+                }
+
+                currentBalance_B += monthlyIncrease;
+            }
+
+            if (money_C > money_A)
+            {
+                while (!cFound)
+                {
+                    month_C++;
+
+                    double monthlyIncrease = currentBalance_C * 0.02;
+                    currentBalance_C += monthlyIncrease;
+
+                    if (currentBalance_C > money_C)
+                    {
+                        cFound = true;
+                    }
+                }
+            }
+
+            string output_months_B = "мес€ц";
+
+            string output_months_C = "мес€цев";
+            if (month_C % 10 == 1)
+            {
+                output_months_C = "мес€ц";
+            }
+            if (month_C % 10 >= 2 && month_C % 10 <= 4)
+            {
+                output_months_C = "мес€ца";
+            }
+            if (month_C % 100 >= 11 && month_C % 100 <= 20)
+            {
+                output_months_C = "мес€цев";
+            }
+
+            output = $"”величение превысит {money_B} руб. за {month_B}-й {output_months_B}.\n" + $"¬клад превысит {money_C} руб. через {month_C} {output_months_C}.";
+
+            return output;
+        }
     }
 }
